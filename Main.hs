0f964a49c1fedc9d196173dc29ded5d30bd31faf
HunterGLapp <hunterlapp@gmail.com>
2018-03-28 14:10:26 -0600

    finished minimal implementation, ignoring Control for now

--- Main.hs
+++ Main.hs
@@ -1,9 +1,17 @@
 module Main where
 import Euterpea
+import HSoM.Examples.EuterpeaExamples
 import NegativeHarmony
+
+testMel :: Music Pitch
+testMel = line $ map ($qn) [c 4, e 4, g 4, b 4,
+                            a 4, f 4, d 4, f 4,
+                            e 4, e 4, f 4, f 4, g 4]
 
 main :: IO ()
 main = do
-  putStrLn "Bleh"
-  playDev 2 $ c 4 qn
+  putStrLn "Test melody:"
+  playDev 2 testMel
+  putStrLn "Test melody negated:"
+  playDev 2 (negateHarm (C, 4) testMel)
   return ()
--- NegativeHarmony.hs
+++ NegativeHarmony.hs
@@ -1,13 +1,28 @@
 module NegativeHarmony where
 import Euterpea
 
-negatePitchAround :: Pitch -> Pitch -> Pitch
-negatePitchAround tonic p = reflect p (getPivots tonic)
+negateHarm :: Pitch -> Music Pitch -> Music Pitch
+negateHarm tonic (Prim pr)       = Prim (negatePr tonic pr)
+negateHarm tonic (m1 :+: m2)     = (negateHarm tonic m1) :+:
+                                   (negateHarm tonic m2)
+negateHarm tonic (m1 :=: m2)     = (negateHarm tonic m1) :=:
+                                   (negateHarm tonic m2)
+negateHarm tonic (Modify ctrl m) = Modify ctrl (negateHarm tonic m)
 
-getPivots :: Pitch -> AbsPitch
-getPivots  p = absPitch p + 3
+negatePr :: Pitch ->
+            Primitive Pitch ->
+            Primitive Pitch            
+negatePr _     (Rest dur)       = Rest dur
+negatePr tonic (Note dur pitch) = Note dur (negateP tonic pitch)
 
-reflect :: Pitch -> AbsPitch -> Pitch
-reflect orig pivot = pitch $ pivot
+negateP :: Pitch -> Pitch -> Pitch
+negateP tonic p = reflectP p (getPivot tonic)
+
+getPivot :: Pitch -> AbsPitch -- just returns the minor third
+getPivot  p = absPitch p + 3  -- actual pivot is b/w m3 and M3
+                             
+              
+reflectP :: Pitch -> AbsPitch -> Pitch
+reflectP orig pivot = pitch $ pivot
                              - (absPitch orig - (pivot + 1))
                       
